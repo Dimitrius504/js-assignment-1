86283690dd665d3be4c05497524844917b2876d6
Dimitrius Mckinnon <200452694@student.georgianc.on.ca>
2023-10-05 19:21:19 -0400

    Added content to skills page from javascript array, added some further styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,55 @@
 (function () {
+    const skillsData = [
+        {
+            "name": "JavaScript",
+            "description": "JavaScript is a versatile programming language commonly used for creating interactive and dynamic web applications. It runs in web browsers and allows for client-side scripting."
+        },
+        {
+            "name": "HTML",
+            "description": "HTML (HyperText Markup Language) is the standard markup language used to create web pages. It structures content on the web and defines elements such as headings, paragraphs, links, and images."
+        },
+        ,
+        {
+            "name": "CSS",
+            "description": "CSS (Cascading Style Sheets) is a stylesheet language used for describing the presentation of a web page written in HTML. It controls the layout, design, and appearance of elements on the web."
+        },
+        {
+            "name": "C#",
+            "description": "C# (C-Sharp) is a versatile, modern programming language developed by Microsoft. It is widely used for building Windows applications, web services, and web applications using ASP.NET framework."
+        },
+        {
+            "name": "ASP.NET",
+            "description": "ASP.NET is an open-source web framework developed by Microsoft for building modern, dynamic, and robust web applications and services. It is built on the .NET framework and supports various programming languages, including C#."
+        },
+        {
+            "name": "PHP",
+            "description": "PHP is a popular server-side scripting language for web development. It creates dynamic web pages, integrates with databases, and has an extensive developer community. Easy to learn and versatile, PHP is widely used to build interactive websites and applications."
+        }
+
+    ];
+
+    // Function to populate the skills list
+    function populateSkillsList() {
+        const skillsList = document.getElementById('skillsList');
+
+        // Loop through the skillsData array and create list items dynamically
+        skillsData.forEach(skill => {
+            const listItem = document.createElement('h2');
+            listItem.textContent = skill.name; // Populate the first-level <li> with the skill name
+
+            // If the skill has a description, create a nested list item for the description
+            if (skill.description) {
+                const descriptionItem = document.createElement('li');
+                descriptionItem.textContent = skill.description; // Populate the nested <li> with the skill description
+                listItem.appendChild(descriptionItem); // Append the nested <li> to the first-level <li>
+            }
+
+            skillsList.appendChild(listItem); // Append the first-level <li> to the skills list
+        });
+    }
+
+    // Call the function to populate the skills lis
+
 
     // Define function to load header from external HTML file
     let loadHeader = () => {
@@ -91,6 +142,7 @@
         // Load header and footer content
         loadHeader();
         loadFooter();
+        populateSkillsList();
 
         // hook up call to action
         const ctaButton = document.querySelector("#aboutPage")
